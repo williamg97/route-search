1ff508e9efd192024fb555df306599ba72a85f94
Will <willgregory97@gmail.com>
2021-04-07 23:15:29 +0100

    Commit first version of app

---
+++ src/main/java/com/wjg/mapsearch/MapsearchApplication.java
@@ -0,0 +1,13 @@
+package com.wjg.mapsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjg/mapsearch/MapsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjg.mapsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
